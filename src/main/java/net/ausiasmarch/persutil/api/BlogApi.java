556cf2b1d4dbc8718010a6b9cb31436daa66ee51
Olga Garcia <olgarciaq11@gmail.com>
2025-11-11 09:50:38 +0100

    numero aleatorio en rango

--- src/main/java/net/ausiasmarch/persutil/api/BlogApi.java
+++ src/main/java/net/ausiasmarch/persutil/api/BlogApi.java
@@ -3,6 +3,7 @@ package net.ausiasmarch.persutil.api;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,13 @@ public class BlogApi {
         int numeroAleatorio = (int)(Math.random()*100) +1;
         return ResponseEntity.ok(numeroAleatorio);
     }
+
+    @GetMapping("/aleatorio/{min}/{max}") //endpoint
+    public ResponseEntity<Integer> aleatorioEnRango(
+        @PathVariable int min,
+        @PathVariable int max) {
+            int numeroAleatorio = (int) (Math.random() * (max - min + 1)) + min;
+            return ResponseEntity.ok(numeroAleatorio);
+        }
 
 }
