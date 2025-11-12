06029a6ab13037ec608a27dcf3f65e19b8bdd365
Olga <olgarciaq11@gmail.com>
2025-11-12 01:11:59 +0100

    ejercicio rafa

--- src/main/java/net/ausiasmarch/persutil/api/BlogApi.java
+++ src/main/java/net/ausiasmarch/persutil/api/BlogApi.java
@@ -8,13 +8,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import net.ausiasmarch.persutil.entity.BlogEntity;
 import net.ausiasmarch.persutil.service.AleatorioService;
 import net.ausiasmarch.persutil.service.BlogService;
 
 @RestController
 @RequestMapping("/blog")
 public class BlogApi {
-    
+
     // inyectar servicio AleatorioService
     @Autowired
     AleatorioService oAleatorioService;
@@ -22,33 +23,48 @@ public class BlogApi {
     @Autowired
     BlogService oBlogService;
 
+    @Autowired
+    BlogEntity oBlogEntity;
+
     @GetMapping("/saludar")
     public ResponseEntity<String> saludar() {
-        return new ResponseEntity<>("\"Hola desde el blog\"", HttpStatus.OK); 
-        //con estas barritas conviertes el texto a JSON
+        return new ResponseEntity<>("\"Hola desde el blog\"", HttpStatus.OK);
+        // con estas barritas conviertes el texto a JSON
     }
 
     @GetMapping("/saludar/buenosdias")
     public ResponseEntity<String> saludarPorLaMañana() {
-        return new ResponseEntity<>("\"Hola, buenos días desde el blog\"", HttpStatus.OK); 
+        return new ResponseEntity<>("\"Hola, buenos días desde el blog\"", HttpStatus.OK);
     }
 
-    @GetMapping("/aleatorio") //endpoint
+    @GetMapping("/aleatorio") // endpoint
     public ResponseEntity<Integer> aleatorio() {
-        int numeroAleatorio = (int)(Math.random()*100) +1;
+        int numeroAleatorio = (int) (Math.random() * 100) + 1;
         return ResponseEntity.ok(numeroAleatorio);
     }
 
-    @GetMapping("/aleatorio/service/{min}/{max}") //endpoint
+    @GetMapping("/aleatorio/service/{min}/{max}") // endpoint
     public ResponseEntity<Integer> aleatorioUsandoServiceEnRango(
-        @PathVariable int min,
-        @PathVariable int max) {
+            @PathVariable int min,
+            @PathVariable int max) {
         return ResponseEntity.ok(oAleatorioService.GenerarNumeroAleatorioEnRango(min, max));
     }
 
     @GetMapping("/rellenauno")
     public ResponseEntity<Long> rellenaBlog() {
         return ResponseEntity.ok(oBlogService.rellenaBlog());
+    }
+
+    @GetMapping("/frasealeatoria")
+    public ResponseEntity<String[]> fraseAleatoria() {
+        String[] frase = oAleatorioService.GenerarFraseAleatoria();
+        oBlogEntity.setTitulo(frase[0] + " " + frase[1] + " " + frase[2] + " " + frase[3] + " " + frase[4]);
+        frase = oAleatorioService.GenerarFraseAleatoria();
+        oBlogEntity.setContenido(frase[0] + " " + frase[1] + " " + frase[2] + " " + frase[3] + " " + frase[4]);
+        frase = oAleatorioService.GenerarFraseAleatoria();
+        oBlogEntity.setEtiquetas(frase[0] + " " + frase[1] + " " + frase[2] + " " + frase[3] + " " + frase[4]);
+        return new ResponseEntity<>(frase, HttpStatus.OK);
+
     }
 
 }
--- src/main/java/net/ausiasmarch/persutil/service/AleatorioService.java
+++ src/main/java/net/ausiasmarch/persutil/service/AleatorioService.java
@@ -4,7 +4,21 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AleatorioService {
+
     public int GenerarNumeroAleatorioEnRango(int min, int max) {
         return (int) (Math.random() * (max - min + 1)) + min;
+    }
+
+    public String[] GenerarFraseAleatoria() {
+        String[] palabras = { "Luz", "Rojo", "Ventana", "Teclado", "Compilar", "Estrella", "Sueño",
+                "Pixel", "Café", "Bosque", "Montaña", "Servidor", "Router", "Clase",
+                "Objeto", "Variable", "Mochila", "Reloj", "Canción", "Fuego",
+                "Agua", "Nube", "Código", "Anochecer", "Universo", };
+        String[] frase = new String[5];
+        for (int i = 0; i < 5; i++) {
+            int indiceAleatorio = GenerarNumeroAleatorioEnRango(0, palabras.length - 1);
+            frase[i] = palabras[indiceAleatorio];
+        }
+        return frase;
     }
 }
