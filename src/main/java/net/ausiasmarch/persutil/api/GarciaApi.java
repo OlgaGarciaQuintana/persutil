e69f5fb69af9d1e942bdcad258f084e824563298
Olga Garcia <olgarciaq11@gmail.com>
2025-11-20 13:00:15 +0100

    añadi los post aleatorios en la api

---
+++ src/main/java/net/ausiasmarch/persutil/api/GarciaApi.java
@@ -0,0 +1,20 @@
+package net.ausiasmarch.persutil.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.ausiasmarch.persutil.service.AleatorioService;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+@RequestMapping("/garcia")
+public class GarciaApi {
+
+    @Autowired
+    AleatorioService oAleatorioService;
+
+    
+    
+}
