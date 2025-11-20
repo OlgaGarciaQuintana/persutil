d3a7aa4bee0402e685558df753dc3b9cb1b940f4
Olga Garcia <olgarciaq11@gmail.com>
2025-11-20 13:51:01 +0100

    garcia api terminado

--- src/main/java/net/ausiasmarch/persutil/api/GarciaApi.java
+++ src/main/java/net/ausiasmarch/persutil/api/GarciaApi.java
@@ -1,10 +1,20 @@
 package net.ausiasmarch.persutil.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import net.ausiasmarch.persutil.entity.GarciaEntity;
 import net.ausiasmarch.persutil.service.AleatorioService;
 import net.ausiasmarch.persutil.service.GarciaService;
 
@@ -14,11 +24,44 @@ import net.ausiasmarch.persutil.service.GarciaService;
 public class GarciaApi {
 
     @Autowired
-    AleatorioService oAleatorioService;
+    GarciaService GarciaService;
 
-    @Autowired
-    GarciaService garciaService;
+     // ----------------------------CRUD---------------------------------
 
-    
+    // obtener post por id
+    @GetMapping("/{id}")
+    public ResponseEntity<GarciaEntity> get(@PathVariable Long id) {
+        return ResponseEntity.ok(GarciaService.get(id));
+    }
+
+    // crear posts
+    @PostMapping("")
+    public ResponseEntity<Long> create(@RequestBody GarciaEntity garciaEntity) {
+        return ResponseEntity.ok(GarciaService.create(garciaEntity));
+    }
+
+    // modificar posts
+    @PutMapping("")
+    public ResponseEntity<Long> update(@RequestBody GarciaEntity garciaEntity) {
+        return ResponseEntity.ok(GarciaService.update(garciaEntity));
+    }
+
+    // borrar posts
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Long> delete(@PathVariable Long id) {
+        return ResponseEntity.ok(GarciaService.delete(id));
+    }
+
+    // listado paginado de posts
+    @GetMapping("")
+    public ResponseEntity<Page<GarciaEntity>> getPage(Pageable oPageable) {
+        return ResponseEntity.ok(GarciaService.getPage(oPageable));
+        
+    }
+
+    @GetMapping("/count")
+    public ResponseEntity<Long> count() {
+        return ResponseEntity.ok(GarciaService.count()); 
+    }
     
 }
