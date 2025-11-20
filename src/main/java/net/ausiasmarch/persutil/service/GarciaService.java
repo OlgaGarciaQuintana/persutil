70a046f8fc42028343e2368a82ca0cf5f5c34c05
Olga Garcia <olgarciaq11@gmail.com>
2025-11-20 13:48:26 +0100

    service terminado

--- src/main/java/net/ausiasmarch/persutil/service/GarciaService.java
+++ src/main/java/net/ausiasmarch/persutil/service/GarciaService.java
@@ -1,5 +1,53 @@
 package net.ausiasmarch.persutil.service;
 
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import net.ausiasmarch.persutil.entity.GarciaEntity;
+import net.ausiasmarch.persutil.repository.GarciaRepository;
+
 public class GarciaService {
     
+    @Autowired
+    GarciaRepository GarciaRepository;
+
+    // ----------------------------CRUD---------------------------------
+    public GarciaEntity get(Long id) {
+        return GarciaRepository.findById(id).orElseThrow(() -> new RuntimeException("Blog not found"));
+    }
+
+    public Long create(GarciaEntity garciaEntity) {
+        garciaEntity.setFechaInicio(LocalDateTime.now());
+        garciaEntity.setFechaFinal(null);
+        GarciaRepository.save(garciaEntity);
+        return garciaEntity.getId();
+    }
+
+    public Long update(GarciaEntity garciaEntity) {
+        GarciaEntity existingGarcia = GarciaRepository.findById(garciaEntity.getId())
+                .orElseThrow(() -> new RuntimeException("Blog not found"));
+        existingGarcia.setTitulo(garciaEntity.getTitulo());
+        existingGarcia.setObjetivo(garciaEntity.getObjetivo());
+        existingGarcia.setFechaInicio(LocalDateTime.now());
+        existingGarcia.setFechaFinal(garciaEntity.getFechaFinal());
+        
+        GarciaRepository.save(existingGarcia);
+        return existingGarcia.getId();
+    }
+
+    public Long delete(Long id) {
+        GarciaRepository.deleteById(id);
+        return id;
+    }
+
+    public Page<GarciaEntity> getPage(Pageable oPageable) {
+        return GarciaRepository.findAll(oPageable);
+    }
+
+    public Long count() {
+        return GarciaRepository.count();
+    }
 }
